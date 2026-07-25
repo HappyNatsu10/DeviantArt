b8d6703476495a41eff0c3af0f9fb3ce35d2adee
HappyNatsu10 <afilakapeter@gmail.com>
2026-07-25 15:04:00 +0100

    Fix syntax in DeviantArtFilters.kt and update repo links

--- app/src/main/java/eu/kanade/tachiyomi/extension/all/deviantart/DeviantArtFilters.kt
+++ app/src/main/java/eu/kanade/tachiyomi/extension/all/deviantart/DeviantArtFilters.kt
@@ -1,4 +1,4 @@
-~package eu.kanade.tachiyomi.extension.all.deviantart
+package eu.kanade.tachiyomi.extension.all.deviantart
 
 import eu.kanade.tachiyomi.source.model.Filter
 import eu.kanade.tachiyomi.source.model.FilterList
